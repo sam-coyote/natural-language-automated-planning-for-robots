e385b9b03501d18e196f0355e257a802979f9436
sam-coyote <samuels.vazquezs@gmail.com>
2014-06-10 16:43:05 -0500

    organization and add tests separately

---
+++ code/tests.py
@@ -0,0 +1,28 @@
+# code dependencies
+import kb_services
+import parsing
+import interpretation
+# network toolkit
+import networkx as nx
+# regular expressions 
+import re
+# drawing
+import networkx.drawing
+import matplotlib.pyplot as plt
+
+G = kb_services.load_semantic_network()
+sentence = "bring valerie something to eat"
+
+# high-level, system-level tests
+
+
+
+# medium-level, feature-level tests
+
+
+
+# low-level tests
+pos, words, sem, nps, solved = interpretation.sentence_grounder(G, sentence)	
+print pos, words, sem, nps, solved
+# knowledge base
+#print 'get all attributes and values: ', kb_services.get_attribute(G, 'sam', 'shape')
